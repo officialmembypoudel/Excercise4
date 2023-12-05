962d3232245de6ddf318b18b0faf0cae6fe43f7e
Memby Poudel <officialmembypoudel@gmail.com>
2023-12-05 21:44:22 +0545

    First Commit

---
+++ Excercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Excercise4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Running, Program!");
+
+        Circle cir = new Circle(5);
+
+        double area = cir.CalculateArea();
+
+        Console.WriteLine($"The area of given circle is {area}");
+
+        Console.ReadKey();
+    }
+}
+
---
+++ Excercise4/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Excercise4
+{
+	public class Shape
+	{
+		public virtual double CalculateArea()
+		{
+			return 0;
+		}
+	}
+
+	public class Circle : Shape
+	{
+		private double radius;
+		private double pi = 3.14;
+
+		public Circle(double radius)
+		{ 
+			this.radius = radius;
+		}
+
+        public override double CalculateArea()
+        {
+			return (pi * radius * radius);
+        }
+    }
+}
+
